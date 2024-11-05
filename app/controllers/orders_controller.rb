c2342cf706343512d0f6974d52905cad305113d9
Augusto Lopes <meleu.dev@gmail.com>
2024-11-05 20:05:55 -0300

    OrdersController#add (this is where we got stuck in livecode)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,15 @@
 require_relative '../views/orders_view'
 
 class OrdersController
-  def initialize(order_repository, meal_repository, customer_repository, employee_repository)
+  ##########################################################
+  # NOTE: this was what was messing with our livecode!!! 🤬
+  ##########################################################
+  # initialize MUST receive the repositories in the arguments in the following order:
+  # 1. meal_repo
+  # 2. customer_repo
+  # 3. employee_repo
+  # 4. order_repo   👈 during the livecode order_repo was being passed as the first one
+  def initialize(meal_repository, customer_repository, employee_repository, order_repository)
     @order_repository = order_repository
     @orders_view = OrdersView.new
     @meal_repository = meal_repository
@@ -9,8 +17,46 @@ class OrdersController
     @employee_repository = employee_repository
   end
 
+  def list_undelivered_orders
+    undelivered_orders = @order_repository.undelivered_orders
+    @orders_view.display(undelivered_orders)
+  end
+
   def list_deliveries
     orders = @order_repository.all
     @orders_view.display(orders)
+  end
+
+  def add
+    customer = select_customer
+    meal = select_meal
+    employee = select_employee
+
+    order = Order.new(meal:, customer:, employee:)
+    @order_repository.create(order)
+    list_deliveries
+  end
+
+  private
+
+  def select_meal
+    meals = @meal_repository.all
+    @orders_view.display_meals(meals)
+    index = @orders_view.ask_for_index
+    meals[index]
+  end
+
+  def select_customer
+    customers = @customer_repository.all
+    @orders_view.display_customers(customers)
+    index = @orders_view.ask_for_index
+    customers[index]
+  end
+
+  def select_employee
+    employees = @employee_repository.all_riders
+    @orders_view.display_riders(employees)
+    index = @orders_view.ask_for_index
+    employees[index]
   end
 end
--- app/views/orders_view.rb
+++ app/views/orders_view.rb
@@ -4,4 +4,27 @@ class OrdersView
       puts "#{index + 1}. #{order.employee.username} must deliver #{order.meal.name} to #{order.customer.name}"
     end
   end
+
+  def display_meals(meals)
+    meals.each_with_index do |meal, index|
+      puts "#{index + 1}: #{meal.name} - $#{meal.price}"
+    end
+  end
+
+  def display_customers(customers)
+    customers.each_with_index do |customer, index|
+      puts "#{index + 1}: #{customer.name} - #{customer.address}"
+    end
+  end
+
+  def display_riders(riders)
+    riders.each_with_index do |rider, index|
+      puts "#{index + 1}: #{rider.username}"
+    end
+  end
+
+  def ask_for_index
+    puts "Choose an item:"
+    gets.chomp.to_i - 1
+  end
 end

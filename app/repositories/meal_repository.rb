3b2f660502a43f8a4acecf4b1282fce086fdab60
Augusto Lopes <meleu.dev@gmail.com>
2024-11-05 10:26:52 -0300

    meal_repository.rb

---
+++ app/repositories/meal_repository.rb
@@ -0,0 +1,47 @@
+require 'csv'
+
+require_relative '../models/meal'
+
+class MealRepository
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @meals = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file)
+  end
+
+  def create(meal)
+    meal.id = @next_id
+    @meals << meal
+    @next_id += 1
+    save_csv
+  end
+
+  def all
+    @meals
+  end
+
+  def find(id)
+    @meals.find { |meal| meal.id == id }
+  end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file, headers: :first_row, header_converters: :symbol) do |meal|
+      meal[:id] = meal[:id].to_i
+      meal[:price] = meal[:price].to_i
+      @meals << Meal.new(meal)
+    end
+    @next_id = @meals.last.id + 1 unless @meals.empty?
+  end
+
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << ["id", "name", "price"]
+      @meals.each do |meal|
+        csv << [meal.id, meal.name, meal.price]
+      end
+    end
+  end
+end
